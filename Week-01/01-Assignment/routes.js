95111b4b556a94af7bbce3e32bc5d296adcd1475
gaoliva <33919349+gaoliva@users.noreply.github.com>
2022-01-13 01:59:39 -0600

    From missed commit

---
+++ Week-01/01-Assignment/routes.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Assignment Basics</title></head>');
+        res.write('<body><form action="/create-user" method="POST"><label for="username">User name:</label><input type="text" name="username"><label for="age">Age:</label><input type="text" name="age"><label for="hair-color">Hair color:</label><input type="text" name="hair-color"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/users') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Assignment Basics</title></head>');
+        res.write('<body><ul><li>User 1</li></ul><ul><li>User 2</li></ul></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/create-user' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody.split('=')[1]);
+            res.statusCode = 302;
+            res.setHeader('Content-Type', 'text/html');
+            res.write('<html>');
+            res.write('<head><title>Assignment Basics</title></head>');
+            res.write('<body><ul><li>');
+            res.write(parsedBody.split('=')[1]);
+            res.write('</li></ul></body>');
+            res.write('</html>');
+            res.end();
+        });
+    }
+};
+
+// module.exports = requestHandler;
+
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'Some hard coded text'
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'Some hard coded text';
+
+exports.handler = requestHandler;
