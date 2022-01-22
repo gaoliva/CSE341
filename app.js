599467f714b61bffce682e10288995936110f404
gaoliva <33919349+gaoliva@users.noreply.github.com>
2022-01-22 00:55:44 -0600

    Try port fix again

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const express = require('express');
 const bodyParser = require('body-parser');
