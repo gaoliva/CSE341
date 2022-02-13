d0e8276d83ee5e95019301d5abf7d589f9f719e0
gaoliva <33919349+gaoliva@users.noreply.github.com>
2022-02-12 19:42:26 -0600

    Cleanup for heroku

--- Week-04/Sequelize-module/util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node-complete', 'root', 'PASSWORD REMOVED FOR GIT', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
-
-module.exports = sequelize;
